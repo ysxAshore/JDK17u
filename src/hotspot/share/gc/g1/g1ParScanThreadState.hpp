2ee4fb8877b2cdc68228dc590ab1f220ac9d6615
ysxAshore <19819660125@163.com>
2026-04-16 11:20:06 +0800

    [add]implemented the dispatch task function in multi-thread-hwgc

--- src/hotspot/share/gc/g1/g1ParScanThreadState.hpp
+++ src/hotspot/share/gc/g1/g1ParScanThreadState.hpp
@@ -45,20 +45,21 @@ class G1EvacuationRootClosures;
 class HeapRegion;
 class outputStream;
 
-class G1ParScanThreadState : public CHeapObj<mtGC> {
-  G1CollectedHeap* _g1h;
-  G1ScannerTasksQueue* _task_queue;
+class G1ParScanThreadState : public CHeapObj<mtGC>
+{
+  G1CollectedHeap *_g1h;
+  G1ScannerTasksQueue *_task_queue;
   G1RedirtyCardsLocalQueueSet _rdc_local_qset;
-  G1CardTable* _ct;
-  G1EvacuationRootClosures* _closures;
+  G1CardTable *_ct;
+  G1EvacuationRootClosures *_closures;
 
-  G1PLABAllocator* _plab_allocator;
+  G1PLABAllocator *_plab_allocator;
 
   AgeTable _age_table;
   G1HeapRegionAttr _dest[G1HeapRegionAttr::Num];
   // Local tenuring threshold.
   uint _tenuring_threshold;
-  G1ScanEvacuatedObjClosure  _scanner;
+  G1ScanEvacuatedObjClosure _scanner;
 
   uint _worker_id;
 
@@ -73,9 +74,9 @@ class G1ParScanThreadState : public CHeapObj<mtGC> {
   Tickspan _trim_ticks;
   // Map from young-age-index (0 == not young, 1 is youngest) to
   // surviving words. base is what we get back from the malloc call
-  size_t* _surviving_young_words_base;
+  size_t *_surviving_young_words_base;
   // this points into the array, as we use the first few entries for padding
-  size_t* _surviving_young_words;
+  size_t *_surviving_young_words;
   // Number of elements in the array above.
   size_t _surviving_words_length;
   // Indicates whether in the last generation (old) there is no more space
@@ -86,9 +87,10 @@ class G1ParScanThreadState : public CHeapObj<mtGC> {
   PartialArrayTaskStepper _partial_array_stepper;
   StringDedup::Requests _string_dedup_requests;
 
-  G1CardTable* ct() { return _ct; }
+  G1CardTable *ct() { return _ct; }
 
-  G1HeapRegionAttr dest(G1HeapRegionAttr original) const {
+  G1HeapRegionAttr dest(G1HeapRegionAttr original) const
+  {
     assert(original.is_valid(),
            "Original region attr invalid: %s", original.get_type_str());
     assert(_dest[original.type()].is_valid_gen(),
@@ -97,32 +99,40 @@ class G1ParScanThreadState : public CHeapObj<mtGC> {
   }
 
   size_t _num_optional_regions;
-  G1OopStarChunkedList* _oops_into_optional_regions;
+  G1OopStarChunkedList *_oops_into_optional_regions;
 
-  G1NUMA* _numa;
+  G1NUMA *_numa;
 
   // Records how many object allocations happened at each node during copy to survivor.
   // Only starts recording when log of gc+heap+numa is enabled and its data is
   // transferred when flushed.
-  size_t* _obj_alloc_stat;
+  size_t *_obj_alloc_stat;
 
 public:
-  G1ParScanThreadState(G1CollectedHeap* g1h,
-                       G1RedirtyCardsQueueSet* rdcqs,
+  // @insert: get taskqueue info
+  uintptr_t getTaskQueueElemsBase() { return _task_queue->getTaskQueueElemsBase(); }
+  uintptr_t getTaskQueueBottomAddr() { return _task_queue->getBottomAddr(); }
+  uintptr_t getTaskQueueAgeTopAddr() { return _task_queue->getAgeTopAddr(); }
+
+  // @insert: debug function
+  void dispatch_task_debug(uintptr_t task);
+
+  G1ParScanThreadState(G1CollectedHeap *g1h,
+                       G1RedirtyCardsQueueSet *rdcqs,
                        uint worker_id,
                        uint n_workers,
                        size_t young_cset_length,
                        size_t optional_cset_length);
   virtual ~G1ParScanThreadState();
 
-  void set_ref_discoverer(ReferenceDiscoverer* rd) { _scanner.set_ref_discoverer(rd); }
+  void set_ref_discoverer(ReferenceDiscoverer *rd) { _scanner.set_ref_discoverer(rd); }
 
 #ifdef ASSERT
   bool queue_is_empty() const { return _task_queue->is_empty(); }
 #endif
 
-  void verify_task(narrowOop* task) const NOT_DEBUG_RETURN;
-  void verify_task(oop* task) const NOT_DEBUG_RETURN;
+  void verify_task(narrowOop *task) const NOT_DEBUG_RETURN;
+  void verify_task(oop *task) const NOT_DEBUG_RETURN;
   void verify_task(PartialArrayScanTask task) const NOT_DEBUG_RETURN;
   void verify_task(ScannerTask task) const NOT_DEBUG_RETURN;
 
@@ -132,32 +142,37 @@ public:
   // if the barrier does not filter out the reference for some reason (e.g.
   // p and q are in the same region, p is in survivor, p is in collection set)
   // To be called during GC if nothing particular about p and obj are known.
-  template <class T> void write_ref_field_post(T* p, oop obj);
+  template <class T>
+  void write_ref_field_post(T *p, oop obj);
 
-  template <class T> void enqueue_card_if_tracked(G1HeapRegionAttr region_attr, T* p, oop o) {
+  template <class T>
+  void enqueue_card_if_tracked(G1HeapRegionAttr region_attr, T *p, oop o)
+  {
     assert(!HeapRegion::is_in_same_region(p, o), "Should have filtered out cross-region references already.");
     assert(!_g1h->heap_region_containing(p)->is_young(), "Should have filtered out from-young references already.");
 
 #ifdef ASSERT
-    HeapRegion* const hr_obj = _g1h->heap_region_containing(o);
+    HeapRegion *const hr_obj = _g1h->heap_region_containing(o);
     assert(region_attr.needs_remset_update() == hr_obj->rem_set()->is_tracked(),
            "State flag indicating remset tracking disagrees (%s) with actual remembered set (%s) for region %u",
            BOOL_TO_STR(region_attr.needs_remset_update()),
            BOOL_TO_STR(hr_obj->rem_set()->is_tracked()),
            hr_obj->hrm_index());
 #endif
-    if (!region_attr.needs_remset_update()) {
+    if (!region_attr.needs_remset_update())
+    {
       return;
     }
     size_t card_index = ct()->index_for(p);
     // If the card hasn't been added to the buffer, do it.
-    if (_last_enqueued_card != card_index) {
+    if (_last_enqueued_card != card_index)
+    {
       _rdc_local_qset.enqueue(ct()->byte_for_index(card_index));
       _last_enqueued_card = card_index;
     }
   }
 
-  G1EvacuationRootClosures* closures() { return _closures; }
+  G1EvacuationRootClosures *closures() { return _closures; }
   uint worker_id() { return _worker_id; }
 
   size_t lab_waste_words() const;
@@ -165,20 +180,20 @@ public:
 
   // Pass locally gathered statistics to global state. Returns the total number of
   // HeapWords copied.
-  size_t flush(size_t* surviving_young_words);
+  size_t flush(size_t *surviving_young_words);
 
 private:
   void do_partial_array(PartialArrayScanTask task);
   void start_partial_objarray(G1HeapRegionAttr dest_dir, oop from, oop to);
 
-  HeapWord* allocate_copy_slow(G1HeapRegionAttr* dest_attr,
+  HeapWord *allocate_copy_slow(G1HeapRegionAttr *dest_attr,
                                oop old,
                                size_t word_sz,
                                uint age,
                                uint node_index);
 
   void undo_allocation(G1HeapRegionAttr dest_addr,
-                       HeapWord* obj_ptr,
+                       HeapWord *obj_ptr,
                        size_t word_sz,
                        uint node_index);
 
@@ -187,7 +202,8 @@ private:
                                 markWord old_mark);
 
   // This method is applied to the fields of the objects that have just been copied.
-  template <class T> void do_oop_evac(T* p);
+  template <class T>
+  void do_oop_evac(T *p);
 
   void dispatch_task(ScannerTask task);
 
@@ -197,16 +213,16 @@ private:
   // Returns a non-NULL pointer if successful, and updates dest if required.
   // Also determines whether we should continue to try to allocate into the various
   // generations or just end trying to allocate.
-  HeapWord* allocate_in_next_plab(G1HeapRegionAttr* dest,
+  HeapWord *allocate_in_next_plab(G1HeapRegionAttr *dest,
                                   size_t word_sz,
                                   bool previous_plab_refill_failed,
                                   uint node_index);
 
-  inline G1HeapRegionAttr next_region_attr(G1HeapRegionAttr const region_attr, markWord const m, uint& age);
+  inline G1HeapRegionAttr next_region_attr(G1HeapRegionAttr const region_attr, markWord const m, uint &age);
 
   void report_promotion_event(G1HeapRegionAttr const dest_attr,
                               oop const old, size_t word_sz, uint age,
-                              HeapWord * const obj_ptr, uint node_index) const;
+                              HeapWord *const obj_ptr, uint node_index) const;
 
   void trim_queue_to_threshold(uint threshold);
 
@@ -231,37 +247,38 @@ public:
   oop handle_evacuation_failure_par(oop obj, markWord m);
 
   template <typename T>
-  inline void remember_root_into_optional_region(T* p);
+  inline void remember_root_into_optional_region(T *p);
   template <typename T>
-  inline void remember_reference_into_optional_region(T* p);
+  inline void remember_reference_into_optional_region(T *p);
 
-  inline G1OopStarChunkedList* oops_into_optional_region(const HeapRegion* hr);
+  inline G1OopStarChunkedList *oops_into_optional_region(const HeapRegion *hr);
 };
 
-class G1ParScanThreadStateSet : public StackObj {
-  G1CollectedHeap* _g1h;
-  G1RedirtyCardsQueueSet* _rdcqs;
-  G1ParScanThreadState** _states;
-  size_t* _surviving_young_words_total;
+class G1ParScanThreadStateSet : public StackObj
+{
+  G1CollectedHeap *_g1h;
+  G1RedirtyCardsQueueSet *_rdcqs;
+  G1ParScanThreadState **_states;
+  size_t *_surviving_young_words_total;
   size_t _young_cset_length;
   size_t _optional_cset_length;
   uint _n_workers;
   bool _flushed;
 
- public:
-  G1ParScanThreadStateSet(G1CollectedHeap* g1h,
-                          G1RedirtyCardsQueueSet* rdcqs,
+public:
+  G1ParScanThreadStateSet(G1CollectedHeap *g1h,
+                          G1RedirtyCardsQueueSet *rdcqs,
                           uint n_workers,
                           size_t young_cset_length,
                           size_t optional_cset_length);
   ~G1ParScanThreadStateSet();
 
   void flush();
-  void record_unused_optional_region(HeapRegion* hr);
+  void record_unused_optional_region(HeapRegion *hr);
 
-  G1ParScanThreadState* state_for_worker(uint worker_id);
+  G1ParScanThreadState *state_for_worker(uint worker_id);
 
-  const size_t* surviving_young_words() const;
+  const size_t *surviving_young_words() const;
 };
 
 #endif // SHARE_GC_G1_G1PARSCANTHREADSTATE_HPP
